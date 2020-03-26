bbef5db69e540416d50825161488c921b321ab66
William Broussard <wbroussard@me.com>
2020-03-25 23:49:58 -0500

    main code complete

--- game.js
+++ game.js
@@ -1,209 +1,136 @@
-var questionDisplay = document.getElementById("question");
-var choices = Array.from(document.getElementsByClassName("choice-text"));
-var progressText = document.getElementById("progressText");
-var scoreText = document.getElementById("score");
-var progressBarFull = document.getElementById("progressBarFull");
-var loader = document.getElementById("loader");
-var game = document.getElementById("game");
-var timerBtn = ('timer');
-var displayAllChoices = document.getElementById("choices");
-var acceptingAnswers = false;
-var score = 0;
-var questionCounter = 0;
-var availableQuesions = [];
-var timerId;
-var time = (questions.length)* 10;
-var currentQuestionIndex = 0;
-var timer = document.getElementById("timer");
+var questions = document.getElementById("question");
+var choices = document.getElementsByClassName("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var timer1 = ('time');
+var feedback1 = document.getElementById("feedback");
 var initials = document.getElementById("initials");
 
-//
-//
-//
+var timerId;
+var time = (questions.length) * 15;
+var currentQuestionIndex = 0;
+
+/////////////////////////////////
 
 function startQuiz() {
-    timerId = setInterval(clock, 1000);
-    timerBtn.textContext = time;
+    var startScreen = document.getElementById("start-screen")
+    startScreen.setAttribute("class", "hide");
+
+    questions.removeAttribute("class");
+
+    timerId = setInterval(clockTick, 1000);
+
+    timer1.textContent = time;
+
     getQuestion();
 }
 
-function clock(){
-    // console.log("called clock interval")
-    time --;
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
 
-    timer.textContent = time;
-    // console.log(time);
-    if(time <= 0){
-        alert("time has run out");
-        clearInterval(timerId);
-    }
+    var title1 = document.getElementById("question-title");
+    title1.textContent = currentQuestion.title;
+
+    choices.innerHTML = "";
+
+    currentQuestion.choices.forEach(function (choice, i) {
+        var choiceN = document.createElement("button");
+        choiceN.setAttribute("class", "choice");
+        choiceN.setAttribute("value", choice);
+
+        choiceN.textContent = I + 1 + ". " + choice;
+
+        choiceN.onclick = questionClick;
+
+        choices.appendChild(choiceN);
+    });
 }
-
-// function endGame(){
-//     alert("game has ended");
-// }
-
-// function getQuestion() {
-//     currentQuestion = questions[currentQuestionIndex];
-//     questionDisplay.textContent = currentQuestion.question;
-//     choices.innerHTML = " ";
-
-//     currentQuestion.forEach(function (choice, i) {
-//         var choiceN = document.createElement("button");
-//         choiceN.setAttribute("class", "choice");
-//         choiceN.setAttribute("value", choice);
-
-//         choiceN.textContent = I + 1 + ". " + choice;
-//         choiceN.onclick = questionClick;
-
-//         choices.appendChild(choiceN);
-//     });
-// }
-
-
-// //question click
-// //function to end an reset game
-// //stop timer
-// //save and end score
-// //CONSTANTS
-// const CORRECT_BONUS = 10;
-// const MAX_QUESTIONS = 3;
-// startGame = () => {
-//     questionCounter = 0;
-//     score = 0;
-//     availableQuesions = [...questions];
-//     getNewQuestion();
-//     // game.classList.remove("hidden");
-//     // loader.classList.add("hidden");
-// };
-
-
-
-// // function getNewQuestion (){
-// //     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-// //         localStorage.setItem("mostRecentScore", score);
-// //         return window.location.assign("/end.html");
-// //     }
-// //     questionCounter++;
-// //     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-// //     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-// //     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-// //     currentQuestion = availableQuesions[questionIndex];
-// //     question.innerHTML = currentQuestion.question;
-// //     choices.forEach(choice => {
-// //         const number = choice.dataset["number"];
-// //         choice.innerHTML = currentQuestion["choice" + number];
-// //     });
-// //     availableQuesions.splice(questionIndex, 1);
-// //     acceptingAnswers = true;
-// // };
-// choices.forEach(choice => {
-//     choice.addEventListener("click", e => {
-//         if (!acceptingAnswers) return;
-//         acceptingAnswers = false;
-//         const selectedChoice = e.target;
-//         const selectedAnswer = selectedChoice.dataset["number"];
-//         const classToApply =
-//             selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-//         if (classToApply === "correct") {
-//             incrementScore(CORRECT_BONUS);
-//         }
-//         selectedChoice.parentElement.classList.add(classToApply);
-//         setTimeout(() => {
-//             selectedChoice.parentElement.classList.remove(classToApply);
-//             getNewQuestion();
-//         }, 1000);
-//     });
-// });
-
-
-// incrementScore = num => {
-//     score += num;
-//     scoreText.innerText = score;
-// };
-
-// const username = document.getElementById("username");
-// const saveScoreBtn = document.getElementById("saveScoreBtn");
-// const finalScore = document.getElementById("finalScore");
-// const mostRecentScore = localStorage.getItem("mostRecentScore");
-
-// const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-// const MAX_HIGH_SCORES = 5;
-
-// finalScore.innerText = mostRecentScore;
-
-// username.addEventListener("keyup", () => {
-//     saveScoreBtn.disabled = !username.value;
-// });
-
-// saveHighScore = e => {
-//     console.log("clicked the save button!");
-//     e.preventDefault();
-
-//     const score = {
-//         score: Math.floor(Math.random() * 100),
-//         name: username.value
-//     };
-//     highScores.push(score);
-//     highScores.sort((a, b) => b.score - a.score);
-//     highScores.splice(5);
-
-//     localStorage.setItem("highScores", JSON.stringify(highScores));
-//     window.location.assign("/");
-// };
-
 
 function questionClick(){
-if (this.value !== questions[currentQuestionIndex].answer) {
-    time -= 10;
-    if (time < 0){
+    if (this.value !== questions[currentQuestionIndex].answer){
+    
+    time -= 15;
+    if (time < 0) {
         time = 0;
+    }
+
+    timer1.textContent = time;
+
+    feedback1.textContent = "Wrong!";
+
     } else {
-alert("correct");
-}}}
 
+    feedback1.textContent = "Correct!";
+    }
 
-if (currentQuestionIndex === questions.length){
-    quizEnd();
-} else {
-    getQuestion();
+    feedback1.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedback1.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length){
+        quizEnd();
+    } else {
+        getQuestion();
+    }
 }
-//##############################
-//######  Main Flow #############
-//##############################
-function quizEnd() {
+
+function quizEnd(){
+
     clearInterval(timerId);
-    window.location.href = "highscore.html"
+
+    var endScreen = document.getElementById("end-screen");
+    endScreen.removeAttribute("class");
+
+    var finalScore = document.getElementById("final-score");
+    finalScore.textContent = time;
+
+    questions.setAttribute("class", "hide");
 }
+
+function clockTick(){
+    time--;
+    timer1.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+
+
+
 function saveHighscore() {
-    // get value of input box
+
     var enteredInitials = initials.value.trim();
-    // make sure value wasn't empty
+
     if (enteredInitials !== "") {
-      // get saved scores from localstorage, or if not any, set to empty array
-      var highscores =
-        JSON.parse(window.localStorage.getItem("highscores")) || [];
-      // format new score object for current user
-      var newScore = {
-        score: time,
-        initials: initials
-      };
-      // save to localstorage
-      highscores.push(newScore);
-      window.localStorage.setItem("highscores", JSON.stringify(highscores));
-      // redirect to next page
-      window.location.href = "highscores.html";
+
+        var highscores =
+            JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var newScore = {
+            score: time,
+            initials: enteredInitials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href = "highscores.html";
     }
-  }
-  function checkForEnter(event) {
-    // "13" represents the enter key
+}
+function checkForEnter(event) {
+
     if (event.key === "Enter") {
-      saveHighscore();
+        saveHighscore();
     }
-  }
-  // user clicks button to submit initials
-  submitBtn.onclick = saveHighscore;
-  // user clicks button to start quiz
-  startBtn.onclick = startQuiz;
-  initialsEl.onkeyup = checkForEnter
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter
